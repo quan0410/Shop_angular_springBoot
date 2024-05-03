3d53f95db9edbfcd22e5087a9e50d4024b1bcb0f
Quân <quan4102000@gmail.com>
2024-05-03 14:07:27 +0700

    detail product

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -5,17 +5,27 @@ import {OrderComponent} from "./order/order.component";
 import {OrderConfirmComponent} from "./order-confirm/order-confirm.component";
 import {LoginComponent} from "./login/login.component";
 import {RegisterComponent} from "./register/register.component";
+import {DetailProductComponent} from "./detail-product/detail-product.component";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, HomeComponent, OrderComponent, OrderConfirmComponent, LoginComponent, RegisterComponent],
+  imports: [
+    RouterOutlet,
+    HomeComponent,
+    OrderComponent,
+    OrderConfirmComponent,
+    LoginComponent,
+    RegisterComponent,
+    DetailProductComponent
+  ],
   template: `
     <!--    <app-home></app-home>-->
     <!--    <app-order></app-order>-->
     <!--    <app-order-confirm></app-order-confirm>-->
-<!--    <app-login></app-login>-->
-    <app-register></app-register>
+    <!--    <app-login></app-login>-->
+    <!--    <app-register></app-register>-->
+    <app-detail-product></app-detail-product>
   `,
   styleUrl: './app.component.css'
 })
